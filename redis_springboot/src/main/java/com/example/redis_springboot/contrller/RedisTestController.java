aec42136846629b0b43ba8a9e98bc066bdb4cd44
liuzihao <251867818@qq.com>
2022-07-04 20:41:07 +0800

    f

--- redis_springboot/src/main/java/com/example/redis_springboot/contrller/RedisTestController.java
+++ redis_springboot/src/main/java/com/example/redis_springboot/contrller/RedisTestController.java
@@ -32,32 +32,33 @@ public class RedisTestController {
     public Map<String, String> hashput() throws ParseException {
         Map map=new HashMap<>();
 
-        if(!redisTemplate.opsForHash().putIfAbsent("map2", "k4", "1")){
-            //redis中有缓存
+        SimpleDateFormat formatFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        Object lastCreateTime = redisTemplate.opsForHash().get("map2", "k2");
+        Date createTime = new Date();
 
-            //将未读数+1
-            redisTemplate.opsForHash().increment("map2","k4",1);
-
-            //判断缓存中的信息是否是最新的
-            SimpleDateFormat formatFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
-            Date lastCreateTime = formatFormat.parse(redisTemplate.opsForHash().get("map2", "k2").toString());
-            Date createTime = new Date();
-            if(!lastCreateTime.after(createTime)){
-                map.put("k1", "v1");
-                map.put("k2", formatFormat.format(createTime));
-                map.put("k3", "v3");
-            }
-        }else {
+        if(lastCreateTime == null){
             //redis中没有缓存
             map.put("k1", "value1");
-            map.put("k2", "2022.07.04 19:55:40");
+            map.put("k2", formatFormat.format(createTime));
             map.put("k3", "value3");
+            map.put("k4", "1");
+
+        }else if (formatFormat.parse(lastCreateTime.toString()).before(createTime)){
+            //redis中有缓存 且 缓存的消息是最新消息
+
+            redisTemplate.opsForHash().increment("map2","k4",1);
+        }else {
+            //redis中有缓存 且 缓存的消息不是最新消息
+
+            map.put("k1", "v1");
+            map.put("k2", formatFormat.format(createTime));
+            map.put("k3", "v3");
+            redisTemplate.opsForHash().increment("map2","k4",1);
         }
         redisTemplate.opsForHash().putAll("map2", map);
 
 
         Map map2 = redisTemplate.opsForHash().entries("map2");
-
 
         return map2;
     }
